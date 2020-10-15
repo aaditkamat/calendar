772b421023123d90da769585fd8ccb6c43e4584e
Aadit Kamat <aadit.k12@gmail.com>
2020-10-15 16:57:21 +0800

    Reformat with black

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ app = typer.Typer()
 DATETIME_OBJ = datetime.datetime(1970, 1, 1)
 
 DAY_EMOJIS = {
-    'Monday': ':confounded_face:',
-    'Tuesday': ':smirking_face:',
-    'Wednesdat': ':relieved_face:',
-    'Thursday': ':smiling_face_with_smiling_eyes:',
-    'Friday': ':winking_face_with_tongue:',
-    'Saturday': ':squinting_face_with_tongue:',
-    'Sunday': ':woozy_face:'
+    "Monday": ":confounded_face:",
+    "Tuesday": ":smirking_face:",
+    "Wednesdat": ":relieved_face:",
+    "Thursday": ":smiling_face_with_smiling_eyes:",
+    "Friday": ":winking_face_with_tongue:",
+    "Saturday": ":squinting_face_with_tongue:",
+    "Sunday": ":woozy_face:",
 }
+
 
 @app.command()
 def day(date_string: str = DATETIME_OBJ.now().strftime("%d/%m/%Y")):
     date = DATETIME_OBJ.strptime(date_string, "%d/%m/%Y")
-    day = date.strftime('%A')
+    day = date.strftime("%A")
     day += f" {emoji.emojize(DAY_EMOJIS[day])}"
     if date_string == DATETIME_OBJ.now().strftime("%d/%m/%Y"):
         typer.echo(f"Today is {day}")
@@ -29,8 +30,7 @@ def day(date_string: str = DATETIME_OBJ.now().strftime("%d/%m/%Y")):
 
 @app.command()
 def duration(
-    start_date_string: str,
-    end_date_string: str,
+    start_date_string: str, end_date_string: str,
 ):
     start_date, end_date = (
         DATETIME_OBJ.strptime(start_date_string, "%d/%m/%Y"),
